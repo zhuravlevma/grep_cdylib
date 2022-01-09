22e1041ec2f2ffb3fa716f3058d278bd45375cdf
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-09 20:32:41 +0300

    add struct config

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    search_string: String,
+    filename: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let search_string = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    let content = fs::read_to_string(filename)
+    let content = fs::read_to_string(config.filename)
         .expect("Oops");
     println!("{:?}", content);
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let search_string = args[1].clone();
+    let filename = args[2].clone();
+    Config {search_string, filename}
 }
