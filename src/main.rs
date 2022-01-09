4f600bbad0860d712414e3051de9f8e471bcfc82
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-09 22:05:59 +0300

    add run func, add handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 struct Config {
-    search_string: String,
+    query: String,
     filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        let search_string = args[1].clone();
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Arguments length must be 3");
+        }
+        let query = args[1].clone();
         let filename = args[2].clone();
-        Config {search_string, filename}
+        Ok(Config {query, filename})
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem with parse arguments, {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        println!("Error in app is {}", e);
+        process::exit(1);
+    };
+}
 
-    let content = fs::read_to_string(config.filename)
-        .expect("Oops");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
     println!("{:?}", content);
+    Ok(())
 }
