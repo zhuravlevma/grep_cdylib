a344b59c4516074ee05d1a6b12f3e2b6b7f8ec76
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-09 22:44:22 +0300

    add search and add test

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,39 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
-    println!("{:?}", content);
+
+    for line in search(&config.query, &content) {
+        println!("{:?}", line);
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, productive, fast
+Pick three.
+        ";
+        assert_eq!(
+            vec!["safe, productive, fast"],
+            search(query, contents)
+        )
+    }
 }
