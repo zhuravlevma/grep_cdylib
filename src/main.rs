eea6adfac6ebde199a18c7c950471dd6efe2922f
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-09 20:17:46 +0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let search_string = &args[1];
+    let filename = &args[2];
+
+    let content = fs::read_to_string(filename)
+        .expect("Oops");
+    println!("{:?}", content);
 }
