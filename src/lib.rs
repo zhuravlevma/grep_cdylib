773a9529ad46d419f74e8506d9b7d8fdc5096962
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-11 02:58:11 +0300

    add iterators

--- src/lib.rs
+++ src/lib.rs
@@ -27,24 +27,12 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 
 
